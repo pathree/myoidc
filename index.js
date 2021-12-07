840421a498ac8e2ed823624a0468144911f450d2
pathree <pathree@gmail.com>
2021-12-08 06:32:41 +0800

    add google auth

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
-var server = require("./server");
- 
-server.start();
+const authorization_endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+const token_endpoint = 'https://oauth2.googleapis.com/token';
+const userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo';
+
+const client_id = '584220930515-742jon2tvv8d8ouj040dpob2b6b0g8kg.apps.googleusercontent.com';
+const client_secret = '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';
+const redirect_uri = 'http://www.example.com/oidc/token';
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const path = require('path');
+const serve = require('koa-static');
+const axios = require('axios');
+
+const app = new Koa();
+const router = new Router();
+
+const main = serve(path.join(__dirname + '/public'));
+
+const oauth = async ctx => {
+    console.log('>>> Exchange code for access token and ID token:\n', ctx.request);
+    const code = ctx.request.query.code;
+    console.log('authorization code:', code);
+
+    const tokenResponse = await axios({
+        method: 'post',
+        url: `${token_endpoint}?` + 
+        `code=${code}&` +
+        `client_id=${client_id}&` +
+        `client_secret=${client_secret}&` +
+        `redirect_uri=${redirect_uri}&` +
+        `grant_type=authorization_code`,
+        headers: {
+            accept: 'application/x-www-form-urlencoded'
+        }
+    });
+
+    console.log("<<< Exchange code for access token and ID token, response:\n", tokenResponse.data);
+    const accessToken = tokenResponse.data.access_token;
+    console.log(`access token: ${accessToken}`);
+
+    console.log('>>> Obtaining user profile information:\n');
+
+    const userinfo = await axios({
+        method: 'get',
+        url: `${userinfo_endpoint}`,
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${accessToken}`
+        }
+    });
+  
+    console.log("<<< Obtaining user profile information:\n", userinfo.data);
+    const name = userinfo.data.name;
+
+  ctx.response.redirect(`/welcome.html?name=${name}`);
+};
+
+router.get('/oidc/token', oauth);
+
+app.use(main);
+app.use(router.routes());
+
+app.listen(80);
+console.log("listen on :80 ...");
--- server.js
+++ server.js
@@ -1,32 +1,54 @@
 var http = require("http");
 var url = require('url');
-const { exit } = require("process");
 var events = require('events');
+var util = require('util');
+var fs = require('fs');
 
 var eventEmitter = new events.EventEmitter();
 
-eventEmitter.on('/', function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+eventEmitter.on('/', function(req, res){
+    var query = url.parse(req.url, true).query;
+
+    //var foo = querystring.parse(query)["foo"];
+    //console.log("foo=" + foo);
+    console.log("foo=" + query.foo);
+    console.log("url=" + query.url);
+
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(util.inspect(url.parse(req.url, false)) + "\n");
+    res.write(util.inspect(url.parse(req.url, true)) + "\n");
+    res.end('Hello World\n');
 });
 
-eventEmitter.on('404', function(request, response){
-   response.writeHead(404, {'Content-Type': 'text/plain'});
-   response.end('404 Not Found\n');
+eventEmitter.on('404', function(req, res){
+   res.writeHead(404, {'Content-Type': 'text/plain'});
+   res.end('404 Not Found\n');
 });
 
 function start() {
-    function onRequest(request, response) {
-        console.log("url:" + request.url);
-        var pathname = url.parse(request.url).pathname;
+    function onRequest(req, res) {
+        console.log("url:" + req.url);
+        var pathname = url.parse(req.url).pathname;
         console.log("pathname: " + pathname);
-            
-        if (eventEmitter.listenerCount(request.url) > 0){
-            eventEmitter.emit(request.url, request, response);
+
+        fs.readFile(pathname.substr(1), function (err, data) {
+            if (err) {
+                console.log(err);
+                res.writeHead(404, {'Content-Type': 'text/html'});
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data.toString());
+            }
+            res.end();
+        });
+                    
+        /*if (eventEmitter.listenerCount(pathname) > 0){
+            eventEmitter.emit(pathname, req, res);
         }
         else {
-            eventEmitter.emit('404', request, response);
-        }
+            eventEmitter.emit('404', req, res);
+        }*/
     }
 
     http.createServer(onRequest).listen(8888);
